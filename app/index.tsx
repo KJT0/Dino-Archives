f54ba354b2230619b8f68f7ab0cabd89ad2b1b55
Kevin Joachin Trolese <Trolesekev@gmail.com>
2025-07-31 00:34:06 +0200

    Boilerplate Project
    This is the starter project, further changes may be done in the near future

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <View style={{flexGrow: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Hello World</Text>
+    </View>
+  );
+}
